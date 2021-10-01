7cde81fb2843b147488cabc3e9b46c37f1a220f0
Yuki326 <88820769+Yuki326@users.noreply.github.com>
2021-10-02 02:55:04 +0900

    codesandboxを使った編集 test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React,{Component} from 'react';
-import Counter from './Counter' ;
-import './App.css';
-import ToggleButton from './ToggleButton'
+import React, { Component } from "react";
+import Counter from "./Counter";
+import "./App.css";
+import ToggleButton from "./ToggleButton";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-        <div className= "header">
-          <ToggleButton title="test"/>
-          <a className="gitLink" href="https://github.com/Yuki326">github</a>
-          <a className="react 練習用ページ" href="https://brave-forest-027d0d210.azurestaticapps.net/">react 練習用ページ</a>
+        <div className="header">
+          <ToggleButton title="test" />
+          <a className="gitLink" href="https://github.com/Yuki326">
+            github
+          </a>
+          <a
+            className="react 練習用ページ"
+            href="https://brave-forest-027d0d210.azurestaticapps.net/"
+          >
+            react 練習用ページ
+          </a>
         </div>
         <div className="testArea">
-          <div className="box1"></div>
+          <div className="box1">test</div>
           <div className="box2"></div>
         </div>
         <div className="footer">
           <Counter />
         </div>
       </div>
-    )
+    );
   }
 }
 
